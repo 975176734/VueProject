82f5435edfd63795bf1019ac0208120f7debdc1e
Febrie-111 <3477349668@qq.com>
2020-05-05 13:23:27 +0800

    背景视频和回到顶部

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,16 +3,19 @@
     <TopNav></TopNav>
     <router-view></router-view>
     <LogAndReg></LogAndReg>
+    <Scoll></Scoll>
   </div>
 </template>
 
 <script>
 import LogAndReg from "./components/LogAndReg.vue"
+import Scoll from "./components/Scoll.vue"
 export default {
   //判断用户是否登录
   components:{
     LogAndReg,
     TopNav:()=>import('./components/TopNav.vue'),
+    Scoll
   },
   created(){
     var userName = localStorage.getItem("userName");
